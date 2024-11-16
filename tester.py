5128e880f97ad39a379402328fc9af518188446f
KKimSJ <karunax@gachon.ac.kr>
2024-11-17 03:05:14 +0900

    Update tester.py

--- tester.py
+++ tester.py
@@ -10,12 +10,12 @@ def circle(r):
   area = math.pi * r**2
   return area
 
-calculate the length of hypotenuse(c) a = 3 & b = 4
+#calculate the length of hypotenuse(c) a = 3 & b = 4
 a, b = 3, 4
 c = utils.pythagoras(a, b)
 print('c =', c)
 
-calculate the area of circle with radius r = 10
+#calculate the area of circle with radius r = 10
 r = 10
 area = utils.circle(r)
 print('area =', area)
