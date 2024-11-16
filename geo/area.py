bee0443a8e65927a5073b0bdb1be66bee2fe96f2
KKimSJ <karunax@gachon.ac.kr>
2024-11-17 01:54:43 +0900

    Update utils.py

--- geo/area.py
+++ geo/area.py
@@ -1,7 +1,19 @@
-import math
-
-def circle_area(radius):
-    return math.pi * radius ** 2
-
-def rectangle_area(length, width):
-    return length * width
+def calculate_area(shape, **kwargs):
+    """
+    Calculate the area of different shapes.
+    :param shape: Type of shape ('circle', 'square', 'rectangle')
+    :param kwargs: Additional parameters like radius, length, breadth
+    :return: Area of the shape
+    """
+    if shape == 'circle':
+        radius = kwargs.get('radius', 0)
+        return 3.14159 * radius ** 2
+    elif shape == 'square':
+        side = kwargs.get('side', 0)
+        return side ** 2
+    elif shape == 'rectangle':
+        length = kwargs.get('length', 0)
+        breadth = kwargs.get('breadth', 0)
+        return length * breadth
+    else:
+        raise ValueError("Unsupported shape")
