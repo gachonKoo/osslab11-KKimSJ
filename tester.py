6b697a68d335e49400b43ed81016c35348e1080f
KKimSJ <karunax@gachon.ac.kr>
2024-11-17 02:46:17 +0900

    Update tester.py

--- tester.py
+++ tester.py
@@ -1,11 +1,9 @@
-from geo.utils import calculate_area  # geo 폴더의 utils.py에서 함수 임포트
+from geo.utils import as utils# geo 폴더의 utils.py에서 함수 임포트
 
-def test_functionality():
-    # 테스트 코드
-    radius = 10
-    area = calculate_area("circle", radius=radius)
-    print(f"c = {radius}")
-    print(f"area = {area}")
+a,b=3,4
+c=
+printf('c=',c)
 
-if __name__ == "__main__":
-    test_functionality()
+r=10
+area=
+print('area=',area)
